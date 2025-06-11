f469b5b3e3bc601c8ff5ae615115ce5f95b67fcd
Goddohi <kmam147@gmail.com>
2025-06-11 09:46:28 +0900

    feat : 파일경로 세분화

--- dohiMessageApp/Util/MessageUtil.cs
+++ dohiMessageApp/Util/MessageUtil.cs
@@ -10,11 +10,12 @@ namespace WalkieDohi.Util
 {
     public static class MessageUtil
     {
-        private const string FOLDER_PATH = @"C:\ReceivedFiles";
-        private const string FOLDER_IMAGE_PATH = @"C:\ReceivedFiles\Image";
+        private const string FOLDER_PATH = @"C:\WalkieDohi";
+        private const string FOLDER_FILE_PATH = @"C:\WalkieDohi\ReceivedFiles";
+        private const string FOLDER_IMAGE_PATH = @"C:\WalkieDohi\ReceivedFiles\Image";
         public static string GetFilePath(string FileName)
         {
-            return System.IO.Path.Combine(FOLDER_PATH, FileName);
+            return System.IO.Path.Combine(FOLDER_FILE_PATH, FileName);
         }
         public static string GetImagePath(string FileName)
         {
@@ -25,11 +26,12 @@ namespace WalkieDohi.Util
         {
             if (!Directory.Exists(FOLDER_PATH))
                 Directory.CreateDirectory(FOLDER_PATH);
+            if (!Directory.Exists(FOLDER_FILE_PATH))
+                Directory.CreateDirectory(FOLDER_FILE_PATH);
         }
         public static void CheckImageDrietory()
         {
-            if (!Directory.Exists(FOLDER_PATH))
-                Directory.CreateDirectory(FOLDER_PATH);
+            CheckFileDrietory();
             if (!Directory.Exists(FOLDER_IMAGE_PATH))
                 Directory.CreateDirectory(FOLDER_IMAGE_PATH);
         }
